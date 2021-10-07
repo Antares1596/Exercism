6de2fb79c1cad9b4192d6b5d4d0bd0a2488b05f0
Rohit <rohitc171@gmail.com>
2021-10-07 21:11:59 +0530

    Create TestTrack.java

---
+++ Java/Remote Control Competition/TestTrack.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class TestTrack {
+    public static void race(RemoteControlCar car) {
+        
+        
+    }
+    public static List<ProductionRemoteControlCar> getRankedCars(ProductionRemoteControlCar prc1,                                                                ProductionRemoteControlCar prc2) {
+        List<ProductionRemoteControlCar> list  = new ArrayList<>();
+        list.add(prc2);
+        list.add(prc1);
+            return list;
+        
+    }
+}
