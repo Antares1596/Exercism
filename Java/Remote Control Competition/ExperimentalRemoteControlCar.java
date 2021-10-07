05e1c3110be5c18751e4b01c0c605aae7ef9efa2
Rohit <rohitc171@gmail.com>
2021-10-07 21:09:22 +0530

    Create ExperimentalRemoteControlCar.java

---
+++ Java/Remote Control Competition/ExperimentalRemoteControlCar.java
@@ -0,0 +1,8 @@
+public class ExperimentalRemoteControlCar implements RemoteControlCar {
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the ExperimentalRemoteControlCar.drive() method");
+    }
+    public int getDistanceTravelled() {
+        return 30;
+    }
+}
